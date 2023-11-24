3fda67a659c1f602f50f5ccad80376c39a42cd0e
Abdurrahman ilbeyli <ilbeyli187@gmail.com>
2023-11-25 02:03:58 +0300

    StepMotorLib v1.2
    Use for STM32. Configuring Arduino...

--- StepMotorLib/Step_Lib.h
+++ StepMotorLib/Step_Lib.h
@@ -1,31 +1,42 @@
 /*
  * Step_Lib.h
+ * @brief Stepper motor control library for STM32 and arduino. Use for motor drivers such as DRV8825, A4988, TB6600.
  *
  *  Created on: Jul 12, 2023
- *      Author: CANAVAR-32
+ *      Author: ilbeylia
  */
 
-#include "stm32f4xx.h"
+
 
 #ifndef INC_STEP_LIB_H_
 #define INC_STEP_LIB_H_
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#elif defined(STM32F4)
+#include "stm32f4xx.h"
+#endif
+
 
 typedef struct
 {
-	TIM_HandleTypeDef*	Timer_handle;  // adım için gerekli timer
-	uint32_t			Timer_channel;
+	#if defined (STM32F4)
+		TIM_HandleTypeDef*	Timer_handle;  // adım için gerekli timer
+		uint32_t			Timer_channel;
 
-	GPIO_TypeDef*		dir_GPIOx;
-	uint16_t			dir_GPIO_Pin;
+		GPIO_TypeDef*		dir_GPIOx;
+		uint16_t			dir_GPIO_Pin;
+	#elif defined (ARDUINO)
+		// arduino için gerkli yapı
+	#endif
 
+		struct
+		{
+			uint32_t	sayac;
+			uint32_t	adim;
+			uint32_t	durum;
+		}step_set;
 
-	struct
-	{
-		uint32_t	sayac;
-		uint32_t	adim;
-		uint32_t	durum;
-	}step_set;
 
 
 }step_config;
@@ -44,10 +55,15 @@ typedef enum {
 
 
 void step_init(	step_config* step_config,
+				#if defined (STM32F4)
 				TIM_HandleTypeDef*	Timer_handle,
 				uint32_t			Timer_channel,
 				GPIO_TypeDef*		dir_GPIOx,
-				uint16_t			dir_GPIO_Pin);
+				uint16_t			dir_GPIO_Pin
+				#elif defined (ARDUINO)
+				// arduino için gerkli parametreler
+				#endif
+				);
 
 void step(step_config* step_config, uint32_t adim, uint32_t yon);
 
