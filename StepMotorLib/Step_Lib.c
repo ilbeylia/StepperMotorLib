495f0d2c7d02856b06b7e2d7e30f8a1736f4d84c
Abdurrahman ilbeyli <ilbeyli187@gmail.com>
2023-11-25 02:03:58 +0300

    StepMotorLib v1.2
    Use for STM32. Configuring Arduino...

--- StepMotorLib/Step_Lib.c
+++ StepMotorLib/Step_Lib.c
@@ -1,52 +1,67 @@
 /*
  * Step_Lib.c
- *
+ * @brief Stepper motor control library for STM32 and arduino. Use for motor drivers such as DRV8825, A4988, TB6600.
  *  Created on: Jul 12, 2023
- *      Author: CANAVAR-32
+ *      Author: ilbeylia
  */
 
 #include "Step_Lib.h"
 
 void step_init(	step_config* step_config,
+				#if defined (STM32F4)
 				TIM_HandleTypeDef*	Timer_handle,
 				uint32_t			Timer_channel,
 				GPIO_TypeDef*		dir_GPIOx,
-				uint16_t			dir_GPIO_Pin)
+				uint16_t			dir_GPIO_Pin
+				#elif defined (ARDUINO)
+				// parameter for the arduino
+				#endif
+				)
 {
-	step_config->Timer_handle 	= Timer_handle;
-	step_config->Timer_channel 	= Timer_channel;
-	step_config->dir_GPIOx		= dir_GPIOx;
-	step_config->dir_GPIO_Pin	= dir_GPIO_Pin;
+	#if defined (STM32F4)
+		step_config->Timer_handle 	= Timer_handle;
+		step_config->Timer_channel 	= Timer_channel;
+		step_config->dir_GPIOx		= dir_GPIOx;
+		step_config->dir_GPIO_Pin	= dir_GPIO_Pin;
+	#elif defined (ARDUINO)
+		// for  arduino
+	#endif
 }
 
 void step(step_config* step_config, uint32_t adim, uint32_t yon)
 {
-	if (step_config->step_set.durum == Step_DURDU){
-		step_config->step_set.adim = adim;
-		step_config->step_set.durum = Step_BASLADI;
-		if(yon == Step_ILERI ){
-			HAL_GPIO_WritePin(step_config->dir_GPIOx, step_config->dir_GPIO_Pin, 1);
+	#if defined (STM32F4)
+		if (step_config->step_set.durum == Step_DURDU){
+			step_config->step_set.adim = adim;
+			step_config->step_set.durum = Step_BASLADI;
+			if(yon == Step_ILERI ){
+				HAL_GPIO_WritePin(step_config->dir_GPIOx, step_config->dir_GPIO_Pin, 1);
 
+			}
+			else {
+				HAL_GPIO_WritePin(step_config->dir_GPIOx, step_config->dir_GPIO_Pin, 0);
+
+			}
+
+			if (0 != adim){
+				HAL_TIM_PWM_Start_IT(step_config->Timer_handle, step_config->Timer_channel);
+				__HAL_TIM_SET_COMPARE(step_config->Timer_handle, step_config->Timer_channel, 100);
+			}
 		}
-		else {
-			HAL_GPIO_WritePin(step_config->dir_GPIOx, step_config->dir_GPIO_Pin, 0);
+	#elif defined (ARDUINO)
 
-		}
-
-		if (0 != adim){
-			HAL_TIM_PWM_Start_IT(step_config->Timer_handle, step_config->Timer_channel);
-			__HAL_TIM_SET_COMPARE(step_config->Timer_handle, step_config->Timer_channel, 100);
-		}
-
-
-	}
+	#endif
 }
 
 void step_durdur(step_config* step_config){
 
-	if (step_config->step_set.sayac == step_config->step_set.adim){
-		HAL_TIM_PWM_Stop_IT(step_config->Timer_handle, step_config->Timer_channel);
-		step_config->step_set.durum = Step_DURDU;
-		step_config->step_set.sayac =0;
-	}
+	#if defined (STM32F4)
+		if (step_config->step_set.sayac == step_config->step_set.adim){
+			HAL_TIM_PWM_Stop_IT(step_config->Timer_handle, step_config->Timer_channel);
+			step_config->step_set.durum = Step_DURDU;
+			step_config->step_set.sayac =0;
+		}
+	#elif defined (ARDUINO)
+
+	#endif
 }
